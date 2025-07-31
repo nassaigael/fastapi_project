189ebbbf06603208c91548e9774044214b1c403f
nassai <hei.nassai@gmail.com>
2025-07-31 09:32:30 +0300

    add all response from questio 1 -> 6

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Response, HTTPException, status, Request
+from pydantic import BaseModel
+from datetime import datetime
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse
+from typing import List, Dict, Union
+app = FastAPI()
+
+# Class model
+class Post(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Variable globale pour stocker les posts en mémoire
+posts_db: List[Post] = []
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+#GET/ping Q1
+@app.get("/pong", status_code=status.HTTP_200_OK)
+async def ping():
+    return Response("pong")
+
+#GET/home Q2
+@app.get("/ping", status_code=status.HTTP_200_OK)
+def home():
+    html_content = "<!DOCTYPE html><html><head><title>Home</title></head><body><h1>Welcome home!</h1></body></html>"
+    return HTMLResponse(html_content, content_type="text/html")
+
+#404 NOT FOUND Q3
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, exc: HTTPException):
+    html_content = "<!DOCTYPE html><html><head><title>Not found</title></head><body><h1>404 page not found</h1></body></html>"
+    return HTMLResponse(content=html_content, media_type="text/html", status_code=status.HTTP_404_NOT_FOUND)
+
+
+#POST/posts Q4
+@app.post("/posts", response_model=List[Post], status_code=status.HTTP_201_CREATED)
+async def create_posts(posts: List[Post]):
+    posts_db.extend(posts)
+    return posts_db
+
+#GET/posts Q5
+@app.get("/posts", response_model=List[Post], status_code=status.HTTP_200_OK)
+async def get_all_posts():
+    return posts_db
+
+#PUT/posts Q6
+@app.put("/posts", response_model=List[Post], status_code=status.HTTP_200_OK)
+async def upsert_post(post: Post):
+    found = False
+    for i, existing_post in enumerate(posts_db):
+        if existing_post.title == post.title:
+            posts_db[i] = post
+            found = True
+            break
+    if not found:
+        posts_db.append(post)
+    return posts_db
---
+++ memory.py
@@ -0,0 +1,4 @@
+from typing import List
+from model import Player
+
+memory_players: List[Player] = []
